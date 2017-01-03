353e171cd8ec776c18043f63cd522a13111a7baf
hejg <hejinguo727@163.com>
2017-01-03 18:22:21 +0800

    self-service

--- pages/main/main.js
+++ pages/main/main.js
@@ -6,7 +6,7 @@ Page({
       {title:'接口加载',image:'../../images/inter-load.png',url:'../inter/inter'},
       {title:'任务执行',image:'../../images/task-exec.png',url:'../task/task'},
       {title:'报表加载',image:'../../images/report-load.png',url:'../report/report'},
-      {title:'自助服务',image:'../../images/service-search.png',url:'../base/more'},
+      {title:'自助服务',image:'../../images/service-search.png',url:'../self-service/self-service'},
       {title:'我的文件',image:'../../images/file-list.png',url:'../base/more'},
       {title:'主机监控',image:'../../images/host-monitor.png',url:'../base/more'},
       {title:'热门问题',image:'../../images/hot-question.png',url:'../base/more'},
